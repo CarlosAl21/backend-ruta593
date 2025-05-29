237c10cc32a75cad34c8d0c64ef1e35472868009
FelixMF <105674487+FelixMF@users.noreply.github.com>
2025-05-28 23:36:30 -0500

    Implementacion logica de los mails

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailService } from './mail.service';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+          user: "alexitotn21@gmail.com",
+          pass: "lgqj yijm nuao ydkp",
+        },
+      },
+      defaults: {
+        from: '"Sistema de Reservas" <noreply@example.com>',
+      },
+      template: {
+        dir: join(__dirname, '..', '..', 'src', 'mail', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
